75a8669d877e0339ec5e9bbd6211140615599eb2
RedBlueCarrots <78305845+RedBlueCarrots@users.noreply.github.com>
2024-11-11 21:33:39 +0800

    why wasnt this included in the commit

--- test_door.gd
+++ test_door.gd
@@ -1,8 +1,13 @@
 extends StaticBody2D
 
 var closed := true
+var locked := true
+var id := -1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+    id = Global.next_id
+    Global.next_id += 1
+    print(id)
     pass # Replace with function body.
 
 
@@ -10,8 +15,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
     pass
 
+func unlock_door():
+    locked = false
+
 func toggle_state():
-    if closed:
+    if closed and !locked:
         closed = false
         $Sprite2D.play("Open")
         $CollisionShape2D.disabled = true
@@ -23,3 +31,11 @@ func toggle_state():
         $CollisionShape2D.disabled = false
         $LightOccluder2D.occluder_light_mask = 1
         $Sprite2D2.visible = true
+
+func get_actions() -> Array[String]:
+    if locked:
+        return ["hackable", "pickable"]
+    elif closed:
+        return ["open"]
+    else:
+        return ["close"]
