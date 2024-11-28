9b711f82b855b5f9e6f08f86f2616ca805d7851a
Jonathan <jonathan@ihle.in>
2024-11-28 21:54:47 +0800

    Minor vent fix

--- vent.gd
+++ vent.gd
@@ -1,5 +1,6 @@
 extends usable
 
+var open = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,10 +11,16 @@ func use(player: String, action: String):
     super(player, action)
     #Check for lockpick
     #Change icon to open vent
-    print("using vent")
+    open = true
     $CollisionShape2D.disabled = true
     $Sprite2D2.modulate = Color(0.1, 0.1, 0.1, 1.0)
     
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
     pass
+
+func get_actions() -> Array[String]:
+    if not open:
+        return ["use"]
+    else:
+        return []
